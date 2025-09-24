6ce8a24ac12784d0a6ab55565029304bd9134c1c
22kahz <anzoariandrie@email.com>
2025-09-24 17:45:53 +1000

    Removed redundant directory "Week 9 Homework"

--- Week 9 Homework/vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "vegalitevis/map.vg.json";
-
-vegaEmbed("#map", vg_1).then(function(result) {
-}).catch(console.error);
---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "vegalitevis/map.vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
